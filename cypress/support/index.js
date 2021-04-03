21543bdca275d5cc57653cdd50b93c199fe4c1cf
Roger Baracy <rogerbaracy.s@gmail.com>
2021-04-03 18:13:42 -0300

    Criado o runner action

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -25,7 +25,7 @@ Cypress.on("test:after:run", (test, runnable) => {
     
     let videoName = Cypress.spec.name
     videoName = videoName.replace('/.js.*', '.js')
-    const videoUrl = 'videos/' + videoName + '.mp4'
+    const videoUrl = '../../videos/' + videoName + '.mp4'
 
     addContext({ test }, videoUrl)
 });
---
+++ generate_report_test.sh
@@ -0,0 +1,1 @@
+rm -Rf cypress/results/json/* && npm run cypress:run && npm run report:merge && npm run report:generate
